4cc6c37c2a26808810242ea6e35f8b29c001644b
kelvinc <139802151+spyral-kc@users.noreply.github.com>
2023-08-08 19:40:18 -0400

    Add browser UI

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,45 +1,99 @@
-const computerChoices = ["rock", "paper", "scissors"]
+const computerChoices = ["Rock", "Paper", "Scissor"]
 
+let roundNumber = 1
 let computerScore = 0
 let playerScore = 0
-//
 
-function playRound () {
+const rock = document.querySelector('#rock')
+const paper = document.querySelector('#paper')
+const scissor = document.querySelector('#scissor')
+const reset = document.querySelector('#reset')
 
-let playerSelection = prompt("Pick rock, paper, or scissors.", "").toLowerCase()
-let computerSelection = computerChoices[Math.floor(Math.random() * computerChoices.length)]
+const round = document.querySelector('#round')
+const selection = document.querySelector('#selection')
+const result = document.querySelector('#result')
+const score = document.querySelector('#score')
+const gameResult = document.querySelector('#gameResult')
 
-console.log("Computer:" + computerSelection + "   " + "You:" + playerSelection)
+//-------------------------------------------------------------//
 
-if (computerSelection === "rock" && playerSelection === "scissor" || 
-    computerSelection === "paper" && playerSelection === "rock" ||
-    computerSelection === "scissors" && playerSelection === "paper") {
-    console.log (`You Lose! ${computerSelection} beats ${playerSelection}.`)
-    computerScore = ++computerScore
+function playRound(playerSelection) {
+
+    let computerSelection = computerChoices[Math.floor(Math.random() * 
+                            computerChoices.length)]
+
+    round.textContent = "Round: " + roundNumber
+    selection.textContent = "Computer: " + computerSelection + "   |   " 
+                            + "You: " + playerSelection
+
+    if (computerSelection === "Rock" && playerSelection === "Scissor" || 
+        computerSelection === "Paper" && playerSelection === "Rock" ||
+        computerSelection === "Scissor" && playerSelection === "Paper") {
+        result.textContent = `You Lose! ${computerSelection} 
+                                beats ${playerSelection}.`
+        computerScore = ++computerScore
+    }
+
+    if (computerSelection === "Rock" && playerSelection == "Paper" || 
+        computerSelection === "Paper" && playerSelection === "Scissor" ||
+        computerSelection === "Scissor" && playerSelection === "Rock") {
+        result.textContent = `You Win! ${playerSelection} 
+                                beats ${computerSelection}.`
+        playerScore = ++playerScore
+    }
+
+    if (computerSelection === playerSelection) {
+        result.textContent = `You Tie! ${computerSelection} 
+                                ties with ${playerSelection}.`
+    }   
+
+    score.textContent = "Computer Score: " + computerScore + "   |   " 
+                        + "Your Score: " + playerScore
+
+    roundNumber = ++roundNumber
+}
+
+//-------------------------------------------------------------//
+
+function playGame (roundNumber, playerSelection) {
     
-}
-if (computerSelection === "rock" && playerSelection == "paper" || 
-    computerSelection === "paper" && playerSelection === "scissors" ||
-    computerSelection === "scissors" && playerSelection === "rock") {
-    console.log (`You Win! ${playerSelection} beats ${computerSelection}.`)
-    playerScore = ++playerScore
-}
-if (computerSelection === playerSelection) {
-    console.log (`You Tie! ${computerSelection} ties ${playerSelection}.`) 
-}    
-console.log("Computer Score:" + computerScore + "   " 
-            + "Your Score:" + playerScore)
+    if (roundNumber < 6){
+        playRound (playerSelection)
+    }
+
+    if (roundNumber === 5) {
+        if (playerScore > computerScore) {
+            gameResult.textContent = "YOU WIN THE GAME!"
+        }
+        if (playerScore < computerScore) (
+            gameResult.textContent = "YOU LOSE THE GAME!"
+        )
+        if (playerScore === computerScore) {
+            gameResult.textContent = "YOU TIE THE GAME!"
+        }
+        document.getElementById("rock").disabled = true;
+        document.getElementById("paper").disabled = true;
+        document.getElementById("scissor").disabled = true;
+    } 
 }
 
-function game() {
-    console.log("Round:1")
-    playRound()
-    console.log("Round:2")
-    playRound()
-    console.log("Round:3")
-    playRound()
-    console.log("Round:4")
-    playRound()
-    console.log("Round:5")
-    playRound()
+//-------------------------------------------------------------//
+
+rock.addEventListener('click', () => playGame(roundNumber, "Rock"))
+paper.addEventListener('click', () => playGame(roundNumber, "Paper"))
+scissor.addEventListener('click', () => playGame(roundNumber, "Scissor"))
+reset.addEventListener('click', () => {
+    roundNumber = 1
+    computerScore = 0
+    playerScore = 0
+    round.textContent = ""
+    selection.textContent = ""
+    result.textContent = ""
+    score.textContent = ""
+    gameResult.textContent = ""
+    document.getElementById("rock").disabled = false;
+    document.getElementById("paper").disabled = false;
+    document.getElementById("scissor").disabled = false;
 }
+)
+
